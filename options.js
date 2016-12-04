0b7157181b9e987114d87ee515e55ac90bbd953d
famaridon <famaridon@visiativ.com>
2016-12-04 20:24:17 +0100

    option page

---
+++ options.js
@@ -0,0 +1,25 @@
+// Saves options to chrome.storage
+function save_options() {
+  var redmineAPIKey = document.getElementById('redmineAPIKey').value;
+  chrome.storage.sync.set({
+    redmineAPIKey: redmineAPIKey
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    redmineAPIKey: ''
+  }, function(items) {
+    document.getElementById('redmineAPIKey').value = items.redmineAPIKey;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
