7d296c1cdffe4614585afc31e029110284de3e60
wtoscer <willy@toscer.me>
2016-12-06 14:20:33 +0100

    make generics functions for storage in firefox or chrome

---
+++ system.js
@@ -0,0 +1,17 @@
+function getStorage(value, callback) {
+	if (chrome.storage.sync) {
+		chrom.storage.sync.get(value, callback);
+	}
+	else if (browser.storage.local) {
+		browser.storage.local.get(value, callback);
+	}
+}
+
+function setStorage(value, callback) {
+	if (chrome.storage.sync) {
+		chrome.storage.sync.set(value, callback);
+	}
+	else if (browser.storage.local) {
+		browser.storage.local.get(value, callback);
+	}
+}
