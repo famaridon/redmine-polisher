745f252c8567805a8526874c4031222eaf69d959
wtoscer <wtoscer@visiativ.com>
2017-04-13 14:03:21 +0200

    use browwser and not chrome

--- query-pager.js
+++ query-pager.js
@@ -1,4 +1,4 @@
-chrome.webRequest.onBeforeRequest.addListener(
+browser.webRequest.onBeforeRequest.addListener(
   function(details) {
     if( details.url.indexOf("per_page=") <= 0 ){
       var redirectUrl = details.url;
