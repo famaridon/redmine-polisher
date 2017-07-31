80583b57e76628ab8db574076c6ce1a1abbe5abb
famaridon <famaridon@visiativ.com>
2017-07-31 14:19:18 +0200

    hide default content

--- redmine-issues.js
+++ redmine-issues.js
@@ -1,8 +1,14 @@
 console.info( "Redmine polisher started!" );
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  let main = document.getElementById("main");
+  main.appendChild(document.createElement('app-root'))
+
   let content = document.getElementById("content");
-  content.appendChild(document.createElement('app-root'))
+  content.style.display = "none";
+  let sidebar = document.getElementById("sidebar");
+  sidebar.style.display = "none";
+
 
   chrome.runtime.sendMessage({event: "loadAngular"}, function(response) {
     console.log(JSON.stringify(response));
