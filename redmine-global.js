3b607c4a5119d005cac53f29059709098cb9526b
famaridon <famaridon@visiativ.com>
2017-09-05 09:01:28 +0200

    add only in top menu

--- redmine-global.js
+++ redmine-global.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
     let newTask = $(`<li>
         <a class="new-issue" href="/projects/moovapps-process-team/issues/new?issue[tracker_id]=35">New task</a>
       </li>`);
-  let newIssue = $(".new-issue");
+  let newIssue = $(".main_menu .new-issue");
   newIssue.parent().after(newDefect);
   newIssue.parent().after(newTask);
 });
