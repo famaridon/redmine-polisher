e56b086a2cff9533252f34b2d2e5711bf773b5a5
famaridon <famaridon@visiativ.com>
2017-12-05 15:06:53 +0100

    split charts

--- redmine-charts.js
+++ redmine-charts.js
@@ -57,175 +57,168 @@ async function initCurrentIteration(configuration){
   });
 }
 
-/**
-* create chart with ideal datasets and global burndown
-**/
-async function createBurndownCharts(configuration, $zone) {
-  return new Promise((resolve, reject) => {
-    $zone.append(`<canvas id="burndown"></canvas>`);
-    var ctx = document.getElementById("burndown");
-    // current iteration
-    let redmine_data_Deferred = $.ajax({
-      url: "https://redminecharts.famaridon.com/api/charts/current/burndown",
-    });
-    let current_version_Deferred = $.ajax({
-      url: "https://redminecharts.famaridon.com/api/charts/current",
-    });
-    $.when(current_version_Deferred, redmine_data_Deferred).done((current_version_Deferred_result, redmine_data_Deferred_result) => {
-
-      let redmine_data= redmine_data_Deferred_result[0];
-      let current_version= current_version_Deferred_result[0];
-
-      // build real data
-      var chartjs_data = [];
-      redmine_data.forEach((item) => {
-        chartjs_data.push({x: moment(item.x).toDate(), y: item.y});
-      });
-
-      // build ideal data
-      let start_date = moment(chartjs_data[0].x).clone().startOf('day');
-      let start_point = chartjs_data[0].y;
-      let due_date = moment(current_version.due_date).clone().startOf('day');
-      let iteration_days = due_date.diff(start_date, 'days')+1;
-      let iteration_workindays = 0;
-      for(var i=0; i < iteration_days; i++){
-        let loop_date = start_date.clone().add(i, 'd');
-        if(!(loop_date.day() === 6 || loop_date.day() === 0)){
-          iteration_workindays++;
-        }
-      }
-
-      let ideal_data = [];
-      // we must have a = (Yb-Ya) / (Xb - Xa)
-      let a = (0 - start_point) / (iteration_workindays - 0);
-      let x = 0;
-      for(var i=0; i <= iteration_days; i++){
-        let loop_date = start_date.clone().add(i, 'd');
-        let point;
-        if(i !== 0 && (loop_date.day() === 1 || loop_date.day() === 0)){
-          point = {x: loop_date.toDate(), y: ideal_data[ideal_data.length-1].y};
-        } else {
-          point = {x: loop_date.toDate(), y: a * x++ + start_point};
-        }
-        ideal_data.push(point);
-      }
-
-      var data = {
-        datasets: [{
-          label: "Real",
-          lineTension: 0,
-          data: chartjs_data ,
-          borderColor: "#36A2EB",
-          pointRadius: 0,
-          fill: false
-        },{
-          label: "Ideal",
-          borderColor: '#FF6384',
-          borderDash: [5, 5],
-          data: ideal_data,
-          lineTension: 0,
-          fill: false
-        }]
-      };
-
-      var options = {
-        responsive: true,
-        title:{
-          display:true,
-          text:'burndown'
-        },
-        tooltips: {
-          mode: 'index',
-          intersect: false,
-        },
-        hover: {
-          mode: 'nearest',
-          intersect: true
-        },
-        scales: {
-          xAxes: [{
-            type: 'time',
-            position: 'bottom',
-            time: {
-              format: timeFormat,
-              // round: 'day'
-              tooltipFormat: 'll HH:mm'
-            } ,
-            scaleLabel: {
-              display: true,
-              labelString: 'Date'
-            }
-          }],
-          yAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: 'Points'
-            },
-            ticks: {
-              min: 0,
-              stepSize: 10
-            }
-          }]
-        }
-      };
-
-      var burndown = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-      });
-      resolve(burndown);
-    });
-  });
-
+async function loadCurrentIteration(){
+  return $.ajax({
+    url: "https://redminecharts.famaridon.com/api/charts/current",
+  }).promise();
 }
 
-async function loadBurndownCharts(configuration, $zone) {
-
-  let chart = await createBurndownCharts(configuration, $zone);
-
-  let categories = await $.ajax({
+async function loadCategories(configuration) {
+  return $.ajax({
     method: "GET",
     url: window.location.origin +"/projects/moovapps-process-team/issue_categories.json",
     headers: {
       'X-Redmine-API-Key': configuration.redmineAPIKey
     }
   }).promise();
+}
 
-  categories.issue_categories.forEach(function(category) {
-    $.ajax({
-      url: `https://redminecharts.famaridon.com/api/charts/current/burndown-${category.id}`,
-      timeout: 3000
-    }).done((data) => {
-      var chartjs_data = [];
-      data.forEach((item) => {
-        chartjs_data.push({x: moment(item.x).toDate(), y: item.y});
-      });
-      chart.data.labels.push(category.name);
-      chart.data.datasets.push({
-        label: category.name,
-        lineTension: 0,
-        data: chartjs_data ,
-        borderColor: textToColor(category.name),
-        pointRadius: 0,
-        fill: false
-      });
-      chart.update();
-    });
+async function loadBurndownChartsData(category) {
+  let url = 'https://redminecharts.famaridon.com/api/charts/current/burndown';
+  if(category) {
+    url = url + `-${category.id}`;
+  }
+  return $.ajax({
+    url: url,
+    timeout: 3000
+  }).promise();
+}
+
+function appendCanvas($zone, category) {
+  let chartId = 'burndown';
+  if(category) {
+    chartId = category.id;
+  }
+  $zone.append(`<canvas id="${chartId}"></canvas>`);
+  return document.getElementById(chartId);
+}
+
+/**
+* create chart with ideal datasets and global burndown
+**/
+async function createBurndownCharts(configuration, $zone, category) {
+
+  const ctx = appendCanvas($zone, category);
+
+  const current_version = await loadCurrentIteration();
+  const redmine_data = await loadBurndownChartsData(category) ;
+
+  if( !redmine_data || redmine_data.length === 0 ) {
+    return
+  }
+
+  // build real data
+  var chartjs_data = [];
+  redmine_data.forEach((item) => {
+    chartjs_data.push({x: moment(item.x).toDate(), y: item.y});
   });
+
+  // build ideal data
+  let start_date = moment(chartjs_data[0].x).clone().startOf('day');
+  let start_point = chartjs_data[0].y;
+  let due_date = moment(current_version.due_date).clone().startOf('day');
+  let iteration_days = due_date.diff(start_date, 'days')+1;
+  let iteration_workindays = 0;
+  for(var i=0; i < iteration_days; i++){
+    let loop_date = start_date.clone().add(i, 'd');
+    if(!(loop_date.day() === 6 || loop_date.day() === 0)){
+      iteration_workindays++;
+    }
+  }
+
+  let ideal_data = [];
+  // we must have a = (Yb-Ya) / (Xb - Xa)
+  let a = (0 - start_point) / (iteration_workindays - 0);
+  let x = 0;
+  for(var i=0; i <= iteration_days; i++){
+    let loop_date = start_date.clone().add(i, 'd');
+    let point;
+    if(i !== 0 && (loop_date.day() === 1 || loop_date.day() === 0)){
+      point = {x: loop_date.toDate(), y: ideal_data[ideal_data.length-1].y};
+    } else {
+      point = {x: loop_date.toDate(), y: a * x++ + start_point};
+    }
+    ideal_data.push(point);
+  }
+
+  const data = {
+    datasets: [{
+      label: "Real",
+      lineTension: 0,
+      data: chartjs_data ,
+      borderColor: "#36A2EB",
+      pointRadius: 0,
+      fill: false
+    },{
+      label: "Ideal",
+      borderColor: '#FF6384',
+      borderDash: [5, 5],
+      data: ideal_data,
+      lineTension: 0,
+      fill: false
+    }]
+  };
+
+  let label = 'burndown';
+  if(category) {
+    label = category.name;
+  }
+
+  const options = {
+    responsive: true,
+    title:{
+      display:true,
+      text:label
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        position: 'bottom',
+        time: {
+          format: timeFormat,
+          // round: 'day'
+          tooltipFormat: 'll HH:mm'
+        } ,
+        scaleLabel: {
+          display: true,
+          labelString: 'Date'
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Points'
+        },
+        ticks: {
+          min: 0,
+          stepSize: 10
+        }
+      }]
+    }
+  };
+
+  var burndown = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+  });
+  return burndown;
 }
 
-function textToColor(text){
-  text = text.toUpperCase();
-  let red = charToColor(text.charCodeAt(0));
-  let green = charToColor(text.charCodeAt(1));
-  let blue = charToColor(text.charCodeAt(2));
-  return `rgb(${red},${green},${blue})`
-}
-
-function charToColor(char){
-  let code = char - 65;
-  return parseInt((code / 26) * 255);
+async function loadBurndownCharts(configuration, $zone) {
+  let chart = await createBurndownCharts(configuration, $zone);
+  let categories = await loadCategories(configuration);
+  categories.issue_categories.forEach(function(category) {
+    createBurndownCharts(configuration, $zone, category);
+  });
 }
 
 async function initNextIteration(configuration){
